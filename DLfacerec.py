bf62fefbee54c7f6ec34a83db76fbaef90aca381
Heather Scheer <79060006+NuttyCoder@users.noreply.github.com>
2024-11-21 17:13:21 -0700

    Update DLfacerec.py

--- DLfacerec.py
+++ DLfacerec.py
@@ -2,6 +2,9 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import tensorflow as tf from tensorflow.keras.models 
+import Sequential from tensorflow.keras.layers 
+import Conv2D, MaxPooling2D, Flatten, Dense
 
 def load_images_from_folder(folder):
     images = []
