09f5b5d942af074a9ec244acac62ec7709d60b1c
Heather Scheer <79060006+NuttyCoder@users.noreply.github.com>
2024-12-05 13:20:22 -0700

    Create windowWidgets.py

---
+++ tkinter/windowWidgets.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+from tkinter import ttk
+
+def button_func():
+    print("Button clicked")
+# create a window
+window = tk.Tk()
+window.title('Window and Widgets')
+window.geometry('800x500')
+
+# ttk label
+label = ttk.Label(master = window, text = 'This is a test')
+label.pack()
+
+# tk text
+text = tk.Text(master = window)
+text.pack()
+
+# ttk entry
+entry = ttk.Entry(master = window)
+entry.pack()
+
+#ttk button
+button = ttk.Button(master = window, text = 'A button', command = button_func)
+
+# run
+window.mainloop()
