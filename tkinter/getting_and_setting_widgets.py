e201c66f8efcab0ace00f8eda5432bad98d70659
Heather Scheer <79060006+NuttyCoder@users.noreply.github.com>
2024-12-06 08:17:10 -0700

    Create getting_and_setting_widgets.py

---
+++ tkinter/getting_and_setting_widgets.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+from tkinter import ttk
+
+def button_func():
+    # get the content of the entry
+    entry_text = entry.get
+
+    # update the label
+    # label.configure(text =  'some other text')
+    label['text'] = entry_text
+
+# window
+window = tk.Tk()
+window.title('Getting and setting widgets')
+
+# widgets
+label = ttk.Label(master = window, text = 'Some text')
+label.pack()
+
+entry = ttk.Entry(master = window)
+entry.pack()
+
+button = ttk.Button(master = window, text = 'The button', command = button_func)
+button.pack()
+
+# run
+window.mainloop()
