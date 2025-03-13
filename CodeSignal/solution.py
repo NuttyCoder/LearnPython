8c7804a674b4dcdb5375e84ca49a45b96d168786
Heather Scheer <79060006+NuttyCoder@users.noreply.github.com>
2025-03-13 10:40:32 -0700

    Update solution.py

--- CodeSignal/solution.py
+++ CodeSignal/solution.py
@@ -190,3 +190,17 @@ del travel_dict["Japan"]
 # Displaying the dictionary
 print("Updated Destination List:", travel_dict)
 
+# A dictionary of countries we've visited and their capitals
+visited_capitals = {
+    "Spain": "Madrid",
+    "Thailand": "Bangkok",
+    "Egypt": "Cairo",
+    "Australia": "Sydney",  # This is an incorrect entry
+}
+
+# TODO: Correct the capital of Australia in the dictionary
+visited_capitals["Australia"] = "Canberra"
+
+# Displaying the updated dictionary
+print("Countries and their capitals:", visited_capitals)
+
