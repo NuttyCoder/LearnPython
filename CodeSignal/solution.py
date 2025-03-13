372cfccc2e66bb82a7899abf1380afdc84d08c26
Heather Scheer <79060006+NuttyCoder@users.noreply.github.com>
2025-03-13 13:04:52 -0700

    Update solution.py

--- CodeSignal/solution.py
+++ CodeSignal/solution.py
@@ -496,3 +496,34 @@ if travel_destinations[destination]["visited"]:
     print(f"You have already visited {destination}!")
 else:
     print(f"It seems you had to cancel your trip to {destination}. Hopefully, you can visit {travel_destinations[destination]['capital']} soon!")
+# Let's imagine a traveler who has a dictionary of destinations with some essential travel info
+travel_destinations = {
+    "France": {"capital": "Paris", "visited": False},
+    "Italy": {"capital": "Rome", "visited": True},
+    "Spain": {"capital": "Madrid", "visited": False},
+}
+
+# Check if the traveler is planning to visit Spain this year
+destination = "Spain"
+
+if travel_destinations[destination]["visited"]:
+    print(f"You have already visited {destination}!")
+else:
+    print(f"It seems you haven't visited {destination} yet. Get ready for an exciting adventure in {travel_destinations[destination]['capital']}!")
+
+travel_destinations = {
+    "France": {"capital": "Paris", "visited": False},
+    "Italy": {"capital": "Rome", "visited": True},
+    "Spain": {"capital": "Madrid", "visited": False},
+}
+
+# TODO: Set 'destination' to "Italy".
+destination = "Italy"
+
+# TODO: Use an if statement to check if 'destination' has been visited.
+if travel_destinations[destination]['visited']:
+    print(f"You have already visited {destination}!")
+else:
+    print(f"It seems you had to cancel your trip to {destination}. Hopefully, you can visit {travel_destinations[destinations]['capital']}soon!")
+# Print a message according to the visited status.
+
