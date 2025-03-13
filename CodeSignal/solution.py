b57387935991db0e902ff317100e75438760cbc5
Heather Scheer <79060006+NuttyCoder@users.noreply.github.com>
2025-03-13 10:32:38 -0700

    Update solution.py

--- CodeSignal/solution.py
+++ CodeSignal/solution.py
@@ -163,5 +163,16 @@ capital_cities["USA"] = "Washington D.C."
 # TODO: Access and print the capital city of USA
 print("The capital of the USA is:", capital_cities["USA"])
 
+# Creating a dictionary to hold the country as key and its capital as value
+capital_cities = {
+    "France": "Paris",
+    "Japan": "Tokyo",
+    "Kenya": "Nairobi",
+}
 
+# Adding a new country and its capital to the dictionary
+capital_cities["India"] = "New Delhi"
+
+# Displaying the dictionary
+print("Updated list of countries and their capitals:", capital_cities["India"])
 
