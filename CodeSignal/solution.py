d39a3f19ac4cdcc4173055718cc0be716e7e8cfb
Heather Scheer <79060006+NuttyCoder@users.noreply.github.com>
2025-03-13 11:12:32 -0700

    Update solution.py

--- CodeSignal/solution.py
+++ CodeSignal/solution.py
@@ -267,3 +267,16 @@ airport_codes = {
 
 # TODO: Access and print the city and country that corresponds to a particular airport code.
 print(airport_codes["LHR"]["city"], "-", airport_codes["LHR"]["country"])
+
+# A dictionary to handle airport codes
+airport_codes = {
+    "JFK": {"city": "New York", "country": "USA"},
+    "LAX": {"city": "Los Angeles", "country": "USA"},
+    "LHR": {"city": "London", "country": "UK"},
+    "HND": {"city": "Tokyo", "country": "Japan"},
+    "SYD": {"city": "Sydney", "country": "Australia"}
+}
+
+# TODO: Update 'country' of 'LHR' to 'England'
+airport_codes['LHR']['country'] ='England'
+print(airport_codes)
