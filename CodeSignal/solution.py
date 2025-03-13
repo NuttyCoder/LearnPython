864e37e0061dfa694c8432eacc57138e7b7bc2ac
Heather Scheer <79060006+NuttyCoder@users.noreply.github.com>
2025-03-13 09:11:17 -0700

    Update solution.py

--- CodeSignal/solution.py
+++ CodeSignal/solution.py
@@ -50,5 +50,15 @@ world_tour_countries = ["Italy", "France", "USA", "Brazil", "India", "China"]
 world_tour_countries.append("Australia")
 world_tour_countries.append("Spain")
 
+
 # print the updated tour plan.
+print(world_tour_countries)
+
+# Current plan for the world tour
+world_tour_countries = ["Italy", "France", "USA", "Brazil", "India", "China"]
+
+# TODO: Visit Japan before Italy
+world_tour_countries.insert(0, "Japan")
+# TODO: Visit Spain after all other countries
+world_tour_countries.append("Spain")
 print(world_tour_countries)
