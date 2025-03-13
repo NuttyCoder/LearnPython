579fb6e5288c610c910aa13dffb2b82ab5f109f3
Heather Scheer <79060006+NuttyCoder@users.noreply.github.com>
2025-03-13 09:07:15 -0700

    Create solution.py

---
+++ CodeSignal/solution.py
@@ -0,0 +1,54 @@
+# Our list of destinations
+travel_destinations = ["Paris", "Sydney", "Tokyo", "New York", "London"]
+
+# Todo: Insert "Berlin" at the third position using the insert() function
+travel_destinations.insert(2,"Berlin")
+# Checking the list
+print(travel_destinations)
+
+# Our travel destinations
+travel_destinations = ["Paris", "Sydney", "Tokyo", "New York", "London"]
+
+#Finding the third destination in the list
+third_destination = travel_destinations[2]
+print("The third destination on the list is: " + third_destination)
+
+
+# TODO: Establish a list to store travel destinations
+# Hint: Use square brackets to denote the list: list_name = [elements, ...]
+travel_destinations = ["Oslo", "Copenhagen","Reykjavik","Helsinki","Stockholm"]
+# TODO: Find the first destination on the list
+# Hint: You can access the first element in a list using list_name[0]
+first_destinations = travel_destinations[0]
+last_destinations = travel_destinations [-1]
+# TODO: Find the last destination on the list
+# Hint: Python allows you to find the last item in a list using list_name[-1]
+print("The first and last destinations in the list " + first_destinations,"" "and", ""+ last_destinations)
+
+# Initial list of countries for the world tour.
+world_tour_countries = ["Italy", "France", "USA", "Brazil", "India", "China"]
+
+# Adds "Australia" to the end of the list.
+world_tour_countries.append("Australia")
+
+# Adds "Spain" to the end of the list, following "Australia".
+world_tour_countries.append("Spain")
+
+# Removes "Brazil" from the list.
+world_tour_countries.remove("Brazil")
+
+# Removes "China" from the list.
+world_tour_countries.remove("China")
+
+# Inserts "Japan" at the beginning of the list.
+world_tour_countries.insert(0, "Japan")
+
+# The current planned countries to visit in the world tour
+world_tour_countries = ["Italy", "France", "USA", "Brazil", "India", "China"]
+
+# Adding new countries to visit at the end of the tour
+world_tour_countries.append("Australia")
+world_tour_countries.append("Spain")
+
+# print the updated tour plan.
+print(world_tour_countries)
